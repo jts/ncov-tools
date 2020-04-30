c8e1061e34abc506b28c9460d0affcf7e7747428
Jared Simpson <jared.simpson@gmail.com>
2020-04-30 12:33:23 -0400

    make plots based on ct values stored in a metadata file

--- qc/plot_qc_sequencing.R
+++ qc/plot_qc_sequencing.R
@@ -10,21 +10,25 @@ plot_depth_by_amplicon <- function(df)
         ggsave("qc_sequencing/depth_by_amplicon_sample.pdf", width=15, height=10)
 }
 
-plot_depth_by_amplicon_and_ct <- function(df)
+plot_depth_by_amplicon_and_ct <- function(df, metadata)
 {
-    ggplot(df, aes(x=ct, y=mean_coverage)) + 
+    # merge df with metadata   
+    merged = dplyr::full_join(df, metadata, by = "sample")
+
+    summary <- merged %>% group_by(sample, amplicon_id, ct)  %>% summarise(mean_coverage=mean(depth))
+
+    ggplot(summary, aes(x=ct, y=mean_coverage)) + 
         geom_point() +
         scale_y_log10() + 
         facet_wrap(. ~ amplicon_id) + 
         theme_bw() + 
         theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-        ggsave("qc_sequencing/depth_by_amplicon_ct.pdf", width=15, height=10)
+        ggsave("qc_sequencing/amplicon_depth_by_ct.pdf", width=15, height=10)
 }
 
 plot_fraction_covered_by_amplicon <- function(df)
 {
-    print(head(df))
     ggplot(df, aes(x=sample, y=fraction_covered)) + 
         geom_point() +
         facet_wrap(. ~ amplicon_id) + 
@@ -34,14 +38,20 @@ plot_fraction_covered_by_amplicon <- function(df)
         ggsave("qc_sequencing/covered_by_amplicon.pdf", width=15, height=10)
 }
 
-plot_depth_per_base <- function(df)
+plot_depth_per_base <- function(df, metadata)
 {
+    # merge df with metadata   
+    merged = dplyr::full_join(df, metadata, by = "sample")
+
+    # construct new column containing the facet label
+    merged$label = paste(merged$sample, " Ct: ", merged$ct, sep="")
+
     # bedtools coverage reports the position along the interval, adding start gives
     # us the position along the genome
-    ggplot(df, aes(x=start + position, y=depth)) + 
+    ggplot(merged, aes(x=start + position, y=depth)) + 
         geom_line() +
         scale_y_log10() + 
-        facet_wrap(. ~ sample, ncol=1) + 
+        facet_wrap(. ~ label, ncol=1) + 
         xlab("Genome position (nt)") +
         theme_bw()
         
@@ -65,7 +75,7 @@ plot_alt_frequency <- function(df)
 
 plot_genome_completeness <- function(df)
 {
-    ggplot(df, aes(x=sample_name, y=pct_covered_bases)) +
+    ggplot(df, aes(x=sample, y=pct_covered_bases)) +
         geom_col() +
         xlab("Sample") +
         ylab("Genome Completeness (%)") +
@@ -75,11 +85,26 @@ plot_genome_completeness <- function(df)
         ggsave("qc_analysis/genome_completeness.pdf", width=12, height=8)
 }
 
+plot_genome_completeness_by_ct <- function(df, metadata)
+{
+    # merge df with metadata   
+    merged = dplyr::full_join(df, metadata, by = "sample")
+
+    ggplot(merged, aes(x=ct, y=pct_covered_bases)) +
+        geom_point() +
+        xlab("Ct") +
+        ylab("Genome Completeness (%)") +
+        ylim(0, 100) +
+        theme_bw() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1))
+        
+        ggsave("qc_analysis/genome_completeness_by_ct.pdf", width=12, height=8)
+}
+
 # load all the files matching this name into one data frame
 # from: https://stackoverflow.com/questions/11433432/how-to-import-multiple-csv-files-at-once
 read_glob <- function(dir, pattern) {
     files = list.files(dir, pattern=pattern)
-    print(files)
     read_sample_file <- function(x) {
         fn <- paste(dir, x, sep="/")
         sample_name <- strsplit(x, ".", fixed=T)[[1]][1]
@@ -89,6 +114,14 @@ read_glob <- function(dir, pattern) {
     }
 
     df = do.call(rbind, lapply(files, read_sample_file))
+    return(df)
+}
+
+# read artic-nextflow's QC file
+read_qc_csv <- function(fn) {
+    df <- read.csv(fn)
+    # standardize sample field
+    df$sample <- df$sample_name
     return(df)
 }
 
@@ -104,9 +137,20 @@ if(!interactive()) {
         q()
     }
 
+    # metadata is optional so we make a default table here
+    metadata = data.frame(sample=character(),
+                          ct=double())
+
+    if(length(args) == 2) {
+        metadata <- read.table(args[2], header=T)
+    }
+
     if(args[1] == "depth_by_position") {
         df <- read_glob("qc_sequencing", "*.per_base_coverage.bed")
-        plot_depth_per_base(df)
+        plot_depth_per_base(df, metadata)
+    } else if(args[1] == "amplicon_depth_by_ct") {
+        df <- read_glob("qc_sequencing", "*.per_base_coverage.bed")
+        plot_depth_by_amplicon_and_ct(df, metadata)
     } else if(args[1] == "amplicon_covered_fraction") {
         df <- read_glob("qc_sequencing", "*.amplicon_coverage.bed")
         plot_fraction_covered_by_amplicon(df)
@@ -114,8 +158,10 @@ if(!interactive()) {
         df <- read_glob("qc_sequencing", "*.fpileup.tsv")
         plot_alt_frequency(df)
     } else if(args[1] == "genome_completeness") {
-        df <- read.csv("qc_analysis/merged.qc.csv")
+        df <- read_qc_csv("qc_analysis/merged.qc.csv")
         plot_genome_completeness(df)
+    } else if(args[1] == "genome_completeness_by_ct") {
+        df <- read_qc_csv("qc_analysis/merged.qc.csv")
+        plot_genome_completeness_by_ct(df, metadata)
     }
-
 }
