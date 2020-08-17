e95ada8505d25f675960ce3c8fe05ffd982b8f01
Jared Simpson <jared.simpson@gmail.com>
2020-08-17 11:27:29 -0400

    no longer use a BED file for depth_per_position; create new BED file for amplicon_depth_by_ct

--- qc/plot_qc_sequencing.R
+++ qc/plot_qc_sequencing.R
@@ -1,23 +1,10 @@
-plot_depth_by_amplicon <- function(df, outname)
-{
-    ggplot(df, aes(x=sample, y=mean_coverage)) +
-        geom_point() +
-        scale_y_log10() +
-        facet_wrap(. ~ amplicon_id) +
-        theme_bw() +
-        theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-        ggsave(outname, width=15, height=10)
-}
 
 plot_depth_by_amplicon_and_ct <- function(df, metadata, outname)
 {
     # merge df with metadata
     merged = dplyr::inner_join(df, metadata, by = "sample")
 
-    summary <- merged %>% group_by(sample, amplicon_id, ct)  %>% summarise(mean_coverage=mean(depth))
-
-    ggplot(summary, aes(x=ct, y=mean_coverage)) +
+    ggplot(merged, aes(x=ct, y=mean_depth)) +
         geom_point() +
         scale_y_log10() +
         facet_wrap(. ~ amplicon_id) +
@@ -59,25 +46,6 @@ plot_depth_per_base <- function(df, metadata, outname)
             scale_y_log10() +
             facet_wrap_paginate(. ~ label, nrow=8, ncol=1, page=i) +
             xlab("Genome position (nt)") +
-            theme_bw()
-        print(p)
-    }
-    dev.off()
-}
-
-plot_alt_frequency <- function(df, outname)
-{
-    pdf(outname, width=8, height=24)
-    num_samples = length(unique(df$sample))
-    plots_per_page = 8
-
-    for(i in seq(1, ceiling(num_samples / plots_per_page))) {
-        p <- ggplot(df, aes(x=position, y=alt_frequency, color=depth)) +
-            geom_line() +
-            facet_wrap_paginate(. ~ sample, nrow=plots_per_page, ncol=1, page=i) +
-            #scale_colour_gradient(low = "white", high = "black") +
-            xlab("Genome position (nt)") +
-            ylab("Alt Frequency") +
             theme_bw()
         print(p)
     }
@@ -129,8 +97,6 @@ library(dplyr)
 library(ggforce)
 
 if(!interactive()) {
-    #mean_coverage <- read_glob("qc_sequencing", "*.mean_coverage.bed")
-    #plot_depth_by_amplicon(mean_coverage)
 
     args = commandArgs(trailingOnly=TRUE)
     if(length(args) == 0) {
@@ -162,7 +128,7 @@ if(!interactive()) {
 
     } else if(command == "amplicon_depth_by_ct") {
 
-        df <- read_glob("qc_sequencing", "*.per_base_coverage.bed")
+        df <- read_glob("qc_sequencing", "*.amplicon_depth.bed")
         outname = sprintf("plots/%s_amplicon_depth_by_ct.pdf", prefix)
         plot_depth_by_amplicon_and_ct(df, metadata, outname)
 
@@ -171,12 +137,6 @@ if(!interactive()) {
         df <- read_glob("qc_sequencing", "*.amplicon_coverage.bed")
         outname = sprintf("plots/%s_amplicon_covered_fraction.pdf", prefix)
         plot_fraction_covered_by_amplicon(df, outname)
-
-    } else if(command == "alt_allele_frequency") {
-
-        df <- read_glob("qc_sequencing", "*.fpileup.tsv")
-        outname = sprintf("plots/%s_alt_frequency.pdf", prefix)
-        plot_alt_frequency(df, outname)
 
     } else if(command == "genome_completeness_by_ct") {
         
