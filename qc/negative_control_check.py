9094531ff8d37fc29569b6869f585c7b8423e895
Jared Simpson <jared.simpson@gmail.com>
2020-08-21 13:48:11 -0400

    raise amplicon detection threshold, change FAIL to WARN, rename column

--- qc/negative_control_check.py
+++ qc/negative_control_check.py
@@ -16,20 +16,21 @@ class RegionCoverage:
         if depth >= self.threshold:
             self.covered_positions += 1
 
-    def span(self):
+    def fraction(self):
         return float(self.covered_positions) / float(self.total_positions)
 
     def print_tsv(self):
-        s = "\t".join([str(self.name), str(self.covered_positions), str(self.total_positions), "%.3f" % (self.span())])
+        s = "\t".join([str(self.name), str(self.covered_positions), str(self.total_positions), "%.3f" % (self.fraction())])
         print(s)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--max-coverage', type=int, default=5)
-parser.add_argument('--max-amplicon-span', type=int, default=0.5)
+parser.add_argument('--max-amplicon-coverage', type=int, default=20)
+parser.add_argument('--max-amplicon-fraction', type=int, default=0.5)
 args, files = parser.parse_known_args()
 
 
-print("\t".join(["file", "qc", "genome_covered_bases", "genome_total_bases", "genome_covered_span", "amplicons_detected"]))
+print("\t".join(["file", "qc", "genome_covered_bases", "genome_total_bases", "genome_covered_fraction", "amplicons_detected"]))
 
 for fn in files:
     with open(fn) as f:
@@ -45,19 +46,19 @@ for fn in files:
             #
             genome_coverage.update(position, depth)
             if amplicon_id not in amplicon_coverage:
-                amplicon_coverage[amplicon_id] = RegionCoverage(amplicon_id, args.max_coverage)
+                amplicon_coverage[amplicon_id] = RegionCoverage(amplicon_id, args.max_amplicon_coverage)
             amplicon_coverage[amplicon_id].update(position, depth)
          
         detected_amplicons = list()       
         for amplicon_id in sorted(amplicon_coverage.keys()):
             ac = amplicon_coverage[amplicon_id]
-            if ac.span() > args.max_amplicon_span:
+            if ac.fraction() > args.max_amplicon_fraction:
                 detected_amplicons.append(str(ac.name))
 
         qc = "PASS"
         if len(detected_amplicons) > 0:
             qc = "WARN"
-        if genome_coverage.span() > 0.01:
-            qc = "FAIL"
+        if genome_coverage.fraction() > 0.01:
+            qc = "WARN"
         print("\t".join([fn, qc, str(genome_coverage.covered_positions), str(genome_coverage.total_positions), \
-                         "%.3f" % (genome_coverage.span()), ",".join(detected_amplicons)]))
+                         "%.3f" % (genome_coverage.fraction()), ",".join(detected_amplicons)]))
