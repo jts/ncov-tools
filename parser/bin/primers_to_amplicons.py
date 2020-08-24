6a6b2366651bc08b430819b8a4c0db1ac7efb479
Richard de Borja <richard.deborja@gmail.com>
2020-08-24 19:37:04 -0400

    updated function calls to include newly create primer picking algorithm

--- parser/bin/primers_to_amplicons.py
+++ parser/bin/primers_to_amplicons.py
@@ -7,15 +7,15 @@ import sys
 import argparse
 import ncov.parser.primers as pr
 
-parser = argparse.ArgumentParser(description='Create unique amplicon BED file')
+parser = argparse.ArgumentParser(description='Create amplicon BED file')
 parser.add_argument('-p', '--primers', help='Primer scheme in BED format')
-parser.add_argument('--offset', default=30, help='Primer offset for coordinates')
+parser.add_argument('--offset', default=0, help='Primer offset for coordinates')
 parser.add_argument('-o', '--output', default='out.bed',
                     help='filename to write BED to')
 parser.add_argument('--pattern', default='nCoV-2019_',
                     help='amplicon name pattern')
-parser.add_argument('--remove_primers', action='store_true',
-                    help='remove primer sequences from amplicons')
+parser.add_argument('--bed_type', default='unique_amplicons',
+                    help='type of BED to produce (e.g. full, no_primers, unique-amplicons')
 if len(sys.argv) <= 1:
     parser.print_help(sys.stderr)
     sys.exit('Invalid number of arguments')
@@ -23,16 +23,12 @@ args = parser.parse_args()
 
 primers = pr.read_bed_file(args.primers)
 primer_pairs = pr.create_primer_pairs(primers=primers)
-amplicon_ranges = pr.create_amplicon_range(primer_pairs=primer_pairs,
-                                           pattern=args.pattern)
-if args.remove_primers:
-    amplicons = pr.create_unique_amplicons(amplicons=amplicon_ranges,
-                                                  offset=args.offset)
-else:
-    amplicons = amplicon_ranges
+amplicon_ranges = pr.create_amplicons(primer_pairs=primer_pairs,
+                                      offset=args.offset,
+                                      type=args.bed_type)
 
 with open(args.output, 'w') as file_o:
-    for line in amplicons:
+    for line in amplicon_ranges:
         file_o.write('\t'.join(line))
         file_o.write('\n')
 file_o.close()
